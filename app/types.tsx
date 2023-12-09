0e9a2193a65bc5fa2aeb19fe416ab31a6327ba0d
Noyon-Podder <noyon.vai369@gmail.com>
2023-12-10 02:53:38 +0600

    42 minute complted

--- app/types.tsx
+++ app/types.tsx
@@ -1,7 +1,15 @@
+type TRandomUser = {
+  id: string;
+  name: string;
+  image: string;
+};
 // layout includes
-
 export type TMenuItems = {
   sizeString: string;
   colorString: string;
   iconString: string;
+};
+
+export type TMenuItemFollowComp = {
+  user: TRandomUser;
 };
